451110786b1001205286c11526cc757ad0f3e394
uncleaar <danilyudin723@gmail.com>
2024-03-03 21:33:22 +0700

    add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,30 @@ import { App } from "./app/App";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import * as Sentry from "@sentry/react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StoreProvider } from "./app/providers/store/StoreProvider";
 
 const queryClient = new QueryClient();
+
+Sentry.init({
+  dsn: "https://8374932d67a2b83534d94d543636d554@o4506847553257472.ingest.sentry.io/4506847555354624",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+
+  tracesSampleRate: 1.0,
+
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/pages/InfoPage/InfoPage.tsx
+++ src/pages/InfoPage/InfoPage.tsx
@@ -1,3 +1,3 @@
 export const InfoPage = () => {
-  return <div>InfoPage</div>;
+  return <div>info</div>;
 };
